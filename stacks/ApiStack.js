db1fe2ce987014f045333df1799a2f848d4203ee
Mikhail Drozdovich <mikhail.drozdovich@bluestone.com.au>
2022-03-25 16:15:14 +0300

    Adding a billing API

--- stacks/ApiStack.js
+++ stacks/ApiStack.js
@@ -9,9 +9,11 @@ export default class ApiStack extends sst.Stack {
         const { table } = props;
 
         this.api = new sst.Api(this, 'Api', {
+            defaultAuthorizationType: 'AWS_IAM',
             defaultFunctionProps: {
                 environment: {
                     TABLE_NAME: table.tableName,
+                    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
                 }
             },
             routes: {
@@ -20,6 +22,7 @@ export default class ApiStack extends sst.Stack {
                 'GET /notes': 'src/list.main',
                 'PUT /notes/{id}': 'src/update.main',
                 'DELETE /notes/{id}': 'src/delete.main',
+                'POST   /billing': 'src/billing.main',
             }
         });
 
