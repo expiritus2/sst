308b1e081c5ac07e1bcaea9e32a7393337a9d76c
Mikhail Drozdovich <mikhail.drozdovich@bluestone.com.au>
2022-03-25 23:40:50 +0300

    Building our React app

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -14,6 +14,7 @@ const config = {
         APP_CLIENT_ID: process.env.REACT_APP_USER_POOL_CLIENT_ID,
         IDENTITY_POOL_ID: process.env.REACT_APP_IDENTITY_POOL_ID,
     },
+    STRIPE_KEY: 'pk_test_4IJVw3xzctRro1iC7qn6HuQJ'
 };
 
 export default config;
